983c9415715451deb6a5763b8f59acdd774d9fc2
Victor Matheus <viictorm1995@gmail.com>
2025-03-07 12:21:44 -0300

    Alteração de 100 para 50

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ paragrafo.innerHTML = 'Escolha um número entre 1 e 10: ';*/
 //Essa escrita acima pode ser utilizada, mas é recomendado utilizar função como 'boas práticas'
 //Além do código ficar mais limpo para ler, também fica mais leve
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -17,7 +17,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100: ');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 50: ');
 }
 
 exibirMensagemInicial();
